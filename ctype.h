78d7fe2022eaea4fe3723884338ea15dc114ec17
neko-para <liao.hy@outlook.com>
2017-11-11 20:45:11 +0800

    Add todos, Add ctype

--- ctype.h
+++ ctype.h
@@ -1,6 +1,24 @@
 #ifndef _CTYPE_H_
 #define _CTYPE_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int iscntrl(int ch);
+int isspace(int ch);
+int isupper(int ch);
+int islower(int ch);
+int isalpha(int ch);
 int isdigit(int ch);
+int isxdigit(int ch);
+int isalnum(int ch);
+int ispunct(int ch);
+int isgraph(int ch);
+int isprint(int ch);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
--- ctype.usr.c
+++ ctype.usr.c
@@ -1,5 +1,47 @@
 #include "ctype.h"
 
+#define __RANGE__(a, b, c) ((unsigned)(a - b) <= (c - b))
+
+int iscntrl(int ch) {
+	return __RANGE__(ch, 0, 0x1F) || ch == 0x7F;
+}
+
+int isspace(int ch) {
+	return __RANGE__(ch, '\t', '\r') || ch == ' ';
+}
+
+int isupper(int ch) {
+	return __RANGE__(ch, 'A', 'Z');
+}
+
+int islower(int ch) {
+	return __RANGE__(ch, 'a', 'z');
+}
+
+int isalpha(int ch) {
+	return isupper(ch) || islower(ch);
+}
+
 int isdigit(int ch) {
-	return (unsigned)(ch - '0') < 10;
+	return __RANGE__(ch, '0', '9');
+}
+
+int isxdigit(int ch) {
+	return isdigit(ch) || __RANGE__(ch, 'a', 'f') || __RANGE__(ch, 'A', 'F');
+}
+
+int isalnum(int ch) {
+	return isalpha(ch) || isdigit(ch);
+}
+
+int ispunct(int ch) {
+	return __RANGE__(ch, '!', '/') || __RANGE__(ch, ':', '@') || __RANGE__(ch, '[', '`') || __RANGE__(ch, '{', '~');
+}
+
+int isgraph(int ch) {
+	return __RANGE__(ch, '!', '~');
+}
+
+int isprint(int ch) {
+	return __RANGE__(ch, ' ', '~');
 }
