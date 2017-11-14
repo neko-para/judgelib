07538525c3a7cd96719ee58abf41419d24ceb185
neko-para <liao.hy@outlook.com>
2017-11-15 07:56:04 +0800

    ban linking syscall

--- syscall.h
+++ syscall.h
@@ -3,9 +3,22 @@
 
 #if _JUDGE_BIT_ == 32
 #include <asm/unistd_32.h>
+
+static long syscall(long id, long p1, long p2, long p3) {
+	asm volatile (
+		"movl %0, %%eax;"
+		"movl %1, %%ebx;"
+		"movl %2, %%ecx;"
+		"movl %3, %%edx;"
+		"int $0x80;"
+		:
+		: "m" (id), "m" (p1), "m" (p2), "m" (p3)
+		: "ebx"
+	);
+}
 #else
 #include <asm/unistd_64.h>
-#endif
 #include <unistd.h>
+#endif
 
 #endif
