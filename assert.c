a9f472b9f9dddcc7a116859c9e46fddd5bb260d2
neko-para <liao.hy@outlook.com>
2017-11-18 11:07:53 +0800

    Update assert. print info to stderr.

--- assert.c
+++ assert.c
@@ -3,7 +3,7 @@
 #include "stdlib.h"
 
 int __assert__(int line, const char* file, const char* exp) {
-	printf("Assertion failed: %s, file %s, line %d\n", exp, file, line);
+	fprintf(stderr, "Assertion failed: %s, file %s, line %d\n", exp, file, line);
 	abort();
 	return 0;
 }
