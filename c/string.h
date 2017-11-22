68df637c1a80f58f041914b59409d2c048c41846
neko-para <liao.hy@outlook.com>
2017-11-22 21:58:50 +0800

    fix string.h extern "C"

--- c/string.h
+++ c/string.h
@@ -3,6 +3,10 @@
 
 #include <stddef.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void* memcpy(void* dest, const void* src, size_t n);
 void* memmove(void* dest, const void* src, size_t n);
 char* strcpy(char* dest, const char* src);
@@ -14,5 +18,9 @@ int strcmp(const char* s1, const char* s2);
 int strncmp(const char* s1, const char* s2, size_t n);
 void* memset(void* s, int c, size_t n);
 size_t strlen(const char* s);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
